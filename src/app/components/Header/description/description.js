15139a331cb10b4a8a2e89bb27469e437e7ab06f
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-10-13 23:39:28 +0530

    commit

--- src/app/components/Header/description/description.js
+++ src/app/components/Header/description/description.js
@@ -7,7 +7,7 @@ const DescriptionCard = () => {
               <h1 className=" text-center text-3xl font-bold text-white">Education Mania</h1>
               
               <p className="text-white mt-3 m-4 max-w-4xl mx-auto text-2xl sm:mt-4  ">
-              Welcome to Education Mania, your educational oasis, where learning knows no bounds. Our mission is simple yet profound: to ignite your passion for learning and support you need to excel. Education Mania is an online platform designed for students, teachers, parents, and lifelong learners. Here, you'll discover a world of knowledge that spans a wide spectrum of subjects, from mathematics to literature, through carefully curated articles, videos, tutorials, and interactive courses tailored to learners of all ages and levels. <br/>
+              Welcome to Education Mania, your educational oasis, where learning knows no bounds. Our mission is simple yet profound: to ignite your passion for learning and provide the resources and support you need to excel. Education Mania is an online platform designed for students, teachers, parents, and lifelong learners. Here, you'll discover a world of knowledge that spans a wide spectrum of subjects, from mathematics to literature, through carefully curated articles, videos, tutorials, and interactive courses tailored to learners of all ages and levels. Connect with expert educators who can offer guidance and insights, and access a suite of study tools to enhance your academic journey. <br/>
               <em>Empower Your Mind. Ignite Your Potential. Education Mania awaits.</em>
               </p>
            
--- src/app/components/Header/header.js
+++ src/app/components/Header/header.js
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <>
       <div className="relative w-full h-80 bg-[url('/images/education.jpeg')] bg-cover bg-center flex items-end justify-center">
-        <div className="absolute bottom-0 mb-[-4.5rem]">
-          <div className="rounded-full overflow-hidden h-48 w-48">
+        <div className="absolute bottom-0 mb-[-5.5rem]">
+          <div className="rounded-full overflow-hidden h-56 w-56">
             <Image src={Profile} alt="img" layout="responsive" objectFit="cover" />
           </div>
         </div>
