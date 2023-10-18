35557024b5525093b124daf87c74fd152a0241bb
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-10-18 18:39:11 +0530

    Commit

--- src/app/Carousel/carousel.js
+++ src/app/Carousel/carousel.js
@@ -1,14 +1,11 @@
 "use client"
 import React, { useState, useEffect } from "react";
-import Image from 'next/image'
-
-
-
+import Image from 'next/image';
 
 const ProductCarousel = () => {
   const products = [
     { name: "Web Development", image: "/images/Web.jpeg" },
-    { name: "AI/ML Developement", image: "/images/Ai.jpeg" },
+    { name: "AI/ML Development", image: "/images/Ai.jpeg" },
     { name: "Data Analytics", image: "/images/Data.jpeg" },
     { name: "Mobile App Development", image: "/images/Mobile.jpeg" },
     { name: "Cyber Security", image: "/images/Pl.jpeg" },
@@ -23,7 +20,7 @@ const ProductCarousel = () => {
     }, 5000);
 
     return () => clearInterval(interval);
-  }, []); // Empty dependency array ensures that effect runs once after initial render
+  }, []);
 
   const visibleProducts = products.slice(startIndex, startIndex + 3);
 
@@ -31,7 +28,7 @@ const ProductCarousel = () => {
     <div className="max-w-screen-xl mx-auto mt-2 p-2 rounded-lg">
       <h1 className="text-4xl font-bold mt-8 mb-6 text-center">Our Services</h1>
       <div className="w-full overflow-hidden relative mt-5 rounded-lg shadow-lg">
-        <div className="carousel-root p-8 rounded-lg shadow-lg flex space-x-16 justify-center items-center">
+        <div className="carousel-root p-8 rounded-lg shadow-lg flex space-x-4 md:space-x-8 lg:space-x-16 justify-center items-center">
           {visibleProducts.map((product, index) => (
             <div key={index} className="text-center hover:scale-105">
               <Image
@@ -39,9 +36,9 @@ const ProductCarousel = () => {
                 src={product.image}
                 width={236}
                 height={236}
-                className="w-56 h-56 object-cover rounded-full mb-2"
+                className="w-36 h-36 md:w-48 md:h-48 lg:w-56 lg:h-56 object-cover rounded-full mb-2"
               />
-              <div className="mt-6">{product.name}</div>
+              <div className="mt-6 text-sm md:text-base lg:text-lg">{product.name}</div>
             </div>
           ))}
         </div>
